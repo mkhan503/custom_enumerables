e06be03847e9a54b13295e656896035ac44a8038
mkhan503 <mahah.mujeebkhan@gmail.com>
2022-02-22 11:48:27 +0400

    Recreated enumerables to pass basic tests

---
+++ my_enumerables.rb
@@ -0,0 +1,92 @@
+module Enumerable
+  def my_each
+    for i in (1..self.length)
+      yield(self[i - 1])
+    end
+    self
+  end
+
+  def my_each_with_index
+    for i in (0..self.length - 1)
+      yield(self[i], i)
+    end
+    self
+  end
+
+  def my_select
+    
+    return_array = []
+    for i in (0..self.length - 1)
+      return_array << self[i] if yield(self[i]) == true
+    end
+    return_array
+
+    # using my_each
+    # return_array = []
+    # self.my_each do |element| 
+    # return_array << element if yield(element) == true
+    # end
+    # return_array
+  end
+
+  def my_all?
+    for i in (0..self.length - 1)
+      return false if yield(self[i]) == false
+    end
+    return true
+  end
+    #self.my_each do |element|
+     # return false if yield(element) == false
+    #end
+    #return true
+  #end
+  def my_any?
+    self.my_each do |element|
+      return true if yield(element) == true
+    end
+    return false
+  end
+
+  def my_none?
+    self.my_each do |element|
+      return false if yield(element) == true
+    end
+    return true
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      self.my_each do |element|
+        count += 1 if yield(element) == true
+      end
+    else
+      count = self.length
+    end
+    count
+  end
+
+  def my_map
+    return_array = []
+    self.my_each do |element|
+      return_array << yield(element)
+    end
+    return_array
+  end
+
+  def my_inject(initial_value=0)
+    self.my_each do |element|
+      initial_value = yield(initial_value, element)
+    end
+    initial_value
+  end
+end
+
+
+
+
+class Array
+  include Enumerable
+  
+end
+
